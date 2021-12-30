3a3f0a504ace21636aded4b5036e90fcc8980114
Galiwango Ananiya <galiwango101@gmail.com>
2021-12-30 17:05:18 +0300

    Add functionality to edit/update article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,27 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice]  = "Article was successfully created"
+      flash[:notice]  = "Article was successfully created."
       redirect_to article_path(@article)
     else
       render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to article_path(@article)
+    else
+      render :edit
     end
   end
 
