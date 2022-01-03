b15ed5aa7d734cf1e821e6713788a870fb7d152c
Galiwango Ananiya <galiwango101@gmail.com>
2022-01-03 15:49:05 +0300

    Update username validations and resolve merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-=======
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
->>>>>>> 01d497c756e5873613cea58b954811ee7b603104
 end
