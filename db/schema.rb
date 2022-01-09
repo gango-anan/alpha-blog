d48dd5b66cc26d47a4a69d635d2ba811df090f92
Galiwango Ananiya <galiwango101@gmail.com>
2022-01-09 22:47:05 +0300

    Generate migration to add user_id to articles table and add articles associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_112721) do
+ActiveRecord::Schema.define(version: 2022_01_07_200108) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
